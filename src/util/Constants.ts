3be7a85a574babce4b59030baa9a77c5253cdf47
Flávio Carmo <flavio.carmo@outlook.com.br>
2023-06-08 12:26:23 -0300

    Ajuste de constants

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -3,17 +3,17 @@ dotenv.config();
 
 export const constants = {
     userAdministrator: {
-        ID: 'd740e608-44a3-11ed-8da5-0242ac140002',
-        NAME: 'ADMINISTRADOR',
-        EMAIL: 'administrator@administrator'
+        ID: process.env["DEFAULT_ADMINSTRATOR_USER_ID"],
+        NAME: process.env["DEFAULT_ADMINSTRATOR_USER_NAME"],
+        EMAIL: process.env["DEFAULT_ADMINSTRATOR_USER_EMAIL"]
     },
     profile: {
-        ADMINISTRATOR_ID:   'b8167e32-0f4a-11ed-9043-0242ac120002',
-        USER_ID:            'f803e36e-3c56-11ed-a6d2-0242ac140002',
-        USER_NAME:          'USER'
+        ADMINISTRATOR_ID:   process.env["DEFAULT_ADMINSTRATOR_PROFILE_ID"],
+        USER_ID:            process.env["DEFAULT_USER_PROFILE_ID"],
+        USER_NAME:          process.env["DEFAULT_USER_PROFILE_NAME"]
     },
     confirmationType: {
-        USER_REGISTRATION_SIGN_IN_ID:    'c67edcc0-b112-42fe-b4dc-38941fdc9333',
-        USER_REGISTRATION_SIGN_IN_NAME:  'User confirmation Sign in'
+        USER_REGISTRATION_SIGN_IN_ID:    process.env["DEFAULT_CONFIRMATION_TYPE_USER_ID"],
+        USER_REGISTRATION_SIGN_IN_NAME:  process.env["DEFAULT_CONFIRMATION_TYPE_USER_NAME"]
     }
 };
