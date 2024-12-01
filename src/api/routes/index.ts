01e66f7bf39ab482d2841fe00e49b46dfa0d54f5
Flávio Carmo <flavio.carmo@outlook.com.br>
2024-12-01 10:38:10 -0300

    Melhorias

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import bodyParser from 'body-parser';
 import user from "./User";
 import confirmationType from "./ConfirmationType";
 import profile from "./Profile";
 
 const routes : Router = Router();
-
-routes.use(bodyParser.json());
 
 const routeModules: Router[] = 
 [
